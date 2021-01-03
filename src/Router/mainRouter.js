afbe248bae56f2d593553a2ec2ddf3cb95c1c065
keemtj <keemgreat@gmail.com>
2021-01-03 22:49:24 +0900

    :construction: Work in progress

---
+++ src/Router/mainRouter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const mainRouter = () => {
+  return <div>main router</div>;
+};
+
+export default mainRouter;
---
+++ src/Style/Reset.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const ResetStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default ResetStyle;
