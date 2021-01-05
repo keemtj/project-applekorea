cbd261295d75d3b51b2bcb39c460cbb27e8c1b5f
keemtj <keemgreat@gmail.com>
2021-01-05 17:04:21 +0900

    :construction: Add video and image

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,16 +3,22 @@ import styled from 'styled-components';
 
 const Main = () => {
   return (
-    <>
-      <StVideo src="videos/video.mp4" typeof="video/mp4" autoPlay />
+    <StMain>
+      <StVideo src="videos/video.mp4" typeof="video/mp4" autoPlay={true} />
       <StImg src="images/image.jpg" />
-    </>
+    </StMain>
   );
 };
+
+const StMain = styled.main`
+  width: 100vw;
+  padding: 10rem 10rem;
+`;
 
 const StVideo = styled.video`
   width: 100%;
 `;
+
 const StImg = styled.img`
   width: 100%;
 `;
