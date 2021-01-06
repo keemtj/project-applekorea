2ef553cc9e2e9f65281b26a7803414186b0ec4b0
keemtj <keemgreat@gmail.com>
2021-01-06 21:42:05 +0900

    :construction: Add article

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,12 +11,10 @@ const Header = () => {
         <a>About</a>
       </StNav>
       <StSubNav>
-        <h1>KimTaejin Pro</h1>
-        <StDownloadButton>
-          <StDownloadResume href="/" download="이력서.pdf">
-            구입하기
-          </StDownloadResume>
-        </StDownloadButton>
+        <h1>KimTaejin Pro 13형</h1>
+        <StDownloadResume href="/" download="이력서.pdf">
+          이력서 다운로드
+        </StDownloadResume>
       </StSubNav>
     </StHeader>
   );
@@ -24,7 +22,7 @@ const Header = () => {
 
 const displayStyle = {
   display: 'flex',
-  padding: '1rem 10rem',
+  padding: '2rem 19.4rem',
   'align-items': 'center',
   'justify-content': 'space-between',
 };
@@ -36,35 +34,35 @@ const StHeader = styled.header`
 const StNav = styled.nav`
   ${displayStyle};
   background: ${({ theme }) => theme.color.black};
+  height: 4.4rem;
+  font-size: 1.3rem;
+  font-weight: 300;
+  color: #e1e1e1;
 `;
 
 const StSubNav = styled.nav`
-  position: fixed;
-  z-index: 2;
   ${displayStyle};
-  padding: 2rem 10rem;
   width: 100%;
-  background-color: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
+  height: 5.2rem;
+  border-bottom: 1px solid grey;
+  background: linear-gradient(rgba(29, 29, 31, 0.72), rgba(29, 29, 31, 0.72)),
     ${({ theme }) => theme.color.bgRgba};
-  background-color: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8));
-  backdrop-filter: blur(10px);
-`;
-
-const StDownloadButton = styled.button`
-  border: none;
-  border-radius: 1.2rem;
-  padding: 0.3rem 0.8rem;
-  background-color: ${({ theme }) => theme.color.blue};
-  &:hover {
-    background-color: ${({ theme }) => theme.color.hoverBlue};
-  }
+  backdrop-filter: blur(5px);
+  font-size: 2rem;
+  font-weight: 600;
 `;
 
 const StDownloadResume = styled.a`
   display: block;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  font-size: 1rem;
+  border: none;
+  border-radius: 2rem;
+  padding: 0.5rem 1rem;
+  width: fit-content;
+  height: 2.2rem;
+  background-color: ${({ theme }) => theme.color.blue};
+  &:hover {
+    background-color: ${({ theme }) => theme.color.hoverBlue};
+  }
   &:link {
     color: ${({ theme }) => theme.color.white};
     text-decoration: none;
@@ -73,5 +71,6 @@ const StDownloadResume = styled.a`
     color: ${({ theme }) => theme.color.white};
     text-decoration: none;
   }
+  font-size: 1.1rem;
 `;
 export default Header;
